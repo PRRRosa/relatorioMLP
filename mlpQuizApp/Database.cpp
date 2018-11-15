e54c2394d9e255f28c0e3e3179bc65b825673daa
Leonardo Dalcin <leonardodalcin@outlook.com>
2018-11-15 18:41:37 -0200

    Parallel start.

--- mlpQuizApp/Database.cpp
+++ mlpQuizApp/Database.cpp
@@ -3,102 +3,112 @@
 //
 #include "Database.h"
 
-Database::Database()
-{       
-        {
-            auto description = "Quais as três características essenciais de uma linguagem orientada a objetos?";
-            std::vector<std::string> alternatives {
-                "Herança, Portabilidade e Coesão",
-                "Polimorfismo, Herança e Acoplamento",
-                "Encapsulamento, Herança e Polimorfismo",
-                "Vínculo estático, vínculo dinâmico e polimorfismo"
-            };
-            _questions.push_back(new Question(description, alternatives, 2));
-        }
+std::vector<Question *> randomize_questions(std::vector<Question *> questions) {
+  std::vector<Question *> randomizedQuestions;
+  int i = 0;
+  #pragma omp parallel for
+  for (i; i < (int) size(questions); i++) {
+    randomizedQuestions.push_back(questions[i]);
+  }
 
-        {
-            auto description = "Em \"R\", dado o segmento de código: \nlibrary(functional)\nmult <- function\n"
-                                    "{\nreturn(a*b);\n}\ndobra <- Curry(mult, a=2)\ntripla <- Curry(mult, a=3)\n"
-                                    "\nQual o resultado ao chamarmos \ndobra(10)\ntripla(10)\n";
-            std::vector<std::string> alternatives {
-                "[1]20\n[1]60",
-                "[1]100\n[1]1000",
-                "[1]10\n[1]10",
-                "[1]20\n[1]30"
-            };
-            _questions.push_back(new Question(description, alternatives, 3));
-        }
+  return randomizedQuestions;
+}
 
-        {
-            auto description = "Qual desses algoritmos de coleta de lixo diminui a fragmentação externa e divide o monte em regiões?";
-            std::vector<std::string> alternatives {
-                "Contador de referências",
-                "Pare e copie", 
-                "Marcar e varrer",
-                "Coleção conservadora"
-            };
-            _questions.push_back(new Question(description, alternatives, 1));
-        }
+Database::Database() {
+  {
+    auto description = "Quais as três características essenciais de uma linguagem orientada a objetos?";
+    std::vector<std::string> alternatives{
+            "Herança, Portabilidade e Coesão",
+            "Polimorfismo, Herança e Acoplamento",
+            "Encapsulamento, Herança e Polimorfismo",
+            "Vínculo estático, vínculo dinâmico e polimorfismo"
+    };
+    _questions.push_back(new Question(description, alternatives, 2));
+  }
 
-        {
-            auto description = "Qual desses algoritmos de coleta de lixo libera a memória logo que ela deixa de ser usada ou referenciada?";
-            std::vector<std::string> alternatives {
-                "Contador de referências",
-                "Coleção conservadora",
-                "Pare e copie",
-                "Marcar e varrer"
-            };
-            _questions.push_back(new Question(description, alternatives, 0));
-        }
+  {
+    auto description = "Em \"R\", dado o segmento de código: \nlibrary(functional)\nmult <- function\n"
+                       "{\nreturn(a*b);\n}\ndobra <- Curry(mult, a=2)\ntripla <- Curry(mult, a=3)\n"
+                       "\nQual o resultado ao chamarmos \ndobra(10)\ntripla(10)\n";
+    std::vector<std::string> alternatives{
+            "[1]20\n[1]60",
+            "[1]100\n[1]1000",
+            "[1]10\n[1]10",
+            "[1]20\n[1]30"
+    };
+    _questions.push_back(new Question(description, alternatives, 3));
+  }
 
-        {
-            auto description = "Dada uma hierarquia de classes em que uma classe C herda de B, e B herda de A, dada uma instância de C que vai ser destruída, em qual ordem os destrutores são chamados?";
-            std::vector<std::string> alternatives {
-                "C,B,A",
-                "B,C,A",
-                "A,B,C",
-                "C,A,B"
-            };
-            _questions.push_back(new Question(description, alternatives, 2));
-        }
+  {
+    auto description = "Qual desses algoritmos de coleta de lixo diminui a fragmentação externa e divide o monte em regiões?";
+    std::vector<std::string> alternatives{
+            "Contador de referências",
+            "Pare e copie",
+            "Marcar e varrer",
+            "Coleção conservadora"
+    };
+    _questions.push_back(new Question(description, alternatives, 1));
+  }
 
-        {
-            auto description = "Se dynamic_cast falhar, que valor é retornado?";
-            std::vector<std::string> alternatives {
-                "void", "null",
-                "ponteiro void",
-                "ponteiro null"
-            };
-            _questions.push_back(new Question(description, alternatives, 3));
-        }
+  {
+    auto description = "Qual desses algoritmos de coleta de lixo libera a memória logo que ela deixa de ser usada ou referenciada?";
+    std::vector<std::string> alternatives{
+            "Contador de referências",
+            "Coleção conservadora",
+            "Pare e copie",
+            "Marcar e varrer"
+    };
+    _questions.push_back(new Question(description, alternatives, 0));
+  }
 
-        {
-        auto description = "Como evitar ClassCastExceptions?";
-        std::vector<std::string> alternatives {
-                "Usando instanceOf", "Checando com is-a",
-                "Usando <- com check",
-                "Checando o tipo de conversão"
-        };
-        _questions.push_back(new Question(description, alternatives, 0));
-    }
+  {
+    auto description = "Dada uma hierarquia de classes em que uma classe C herda de B, e B herda de A, dada uma instância de C que vai ser destruída, em qual ordem os destrutores são chamados?";
+    std::vector<std::string> alternatives{
+            "C,B,A",
+            "B,C,A",
+            "A,B,C",
+            "C,A,B"
+    };
+    _questions.push_back(new Question(description, alternatives, 2));
+  }
+
+  {
+    auto description = "Se dynamic_cast falhar, que valor é retornado?";
+    std::vector<std::string> alternatives{
+            "void", "null",
+            "ponteiro void",
+            "ponteiro null"
+    };
+    _questions.push_back(new Question(description, alternatives, 3));
+  }
+
+  {
+    auto description = "Como evitar ClassCastExceptions?";
+    std::vector<std::string> alternatives{
+            "Usando instanceOf", "Checando com is-a",
+            "Usando <- com check",
+            "Checando o tipo de conversão"
+    };
+    _questions.push_back(new Question(description, alternatives, 0));
+  }
 
 
-        _questions.push_back(new Question());
+  _questions.push_back(new Question());
 
+  std::cout << _questions[0]->getDescription() << "/n";
+  _questions = randomize_questions(_questions);
+  std::cout << _questions[0]->getDescription();
 
 }
 
-Database::~Database()
-{
-    delete(&_questions);
+Database::~Database() {
+  delete (&_questions);
 }
 
-Question Database::getQuestion(int index)
-{
-    return *_questions[index];
+Question Database::getQuestion(int index) {
+  return *_questions[index];
 }
 
-int Database::getQuestionsSize()
-{
-    return _questions.size();
+int Database::getQuestionsSize() {
+  return _questions.size();
 }
